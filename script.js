c20585e6840043a31d42245a65c69704b534ba46
Spnarkdnark <samuelfudala@gmail.com>
2022-02-16 18:07:13 -0500

    a bit more javascript functionality, calc kinda works but bugs are creeping

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let display = [];
-let currentValue = [];
-let lastValue = [];
+let currentValue = '';
+let lastValue = '';
 let currentOperand = 'n/a';
 
 let dummyDisplay = document.querySelector('#dummy');
@@ -26,11 +26,11 @@ const division = function(x,y){
 const operate = function(x,y,operand){
    
     switch(operand){
-        case '+': return addition(x,y);
+        case '+': console.log('+'); return addition(x,y);
         break;
-        case '-': return subtraction(x,y);
+        case '-': console.log('-');return subtraction(x,y);
         break;
-        case '*': return multiplication(x,y);
+        case '*': console.log('*');return multiplication(x,y);
         break;
         case '/': return division(x,y);
         break;
@@ -38,12 +38,18 @@ const operate = function(x,y,operand){
     };
 }
 
-const parseOperand = function(x, y, operand){
+const parseOperand = function(operand){
     if (operand === 'c'){
-        currentValue = [];
+        currentValue = '';
+        lastValue = '';
     }
-    else if (operand === '='){
-        operate(x,y,currentOperand);
+    else if (operand === '=' && lastValue){
+        currentValue = operate(Number(lastValue), Number(currentValue), currentOperand);
+    }
+    else{
+        lastValue = currentValue;
+        currentValue = '';
+        currentOperand = operand;
     }
 };
 
@@ -53,14 +59,14 @@ const getAllInputListeners = function(){
             let inputType = item.id[0];
             let inputValue = item.id[1];
             if (inputType === 'o'){
-                let answer = parseOperand(5,3,inputValue);
-                currentOperand = inputValue;
-                console.log(answer);
+                parseOperand(inputValue);
+                dummyDisplay.textContent = currentValue;
+
             }
             else if (inputType === 'v'){
-                currentValue.push(inputValue);
+                currentValue += inputValue;
             }
-            dummyDisplay.textContent = currentValue.join('');
+            dummyDisplay.textContent = currentValue;
             operand.textContent = currentOperand;
 
         },true);});
